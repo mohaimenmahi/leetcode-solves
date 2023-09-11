7575fde74f54e9101e319fa6d0734e8e3944e02d
Mohammad Mohaimen Chowdhury <mmc.dp11@gmail.com>
2023-09-12 00:18:58 +0600

    Time: 11 ms (32.42%) | Memory: 8.9 MB (25.89%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,53 @@
+class Solution {
+private:
+    void backtrack(
+        int r, 
+        int n,
+        unordered_set<int>& col, 
+        unordered_set<int>& neg, 
+        unordered_set<int>& pos, 
+        vector<string>& board,
+        vector<vector<string>>& ans
+    ) {
+        if(r == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for(int c = 0; c < n; c++) {
+            if(col.find(c) != col.end() ||
+                neg.find(r-c) != neg.end() ||
+                pos.find(r+c) != pos.end()
+            ) continue;
+
+            col.insert(c);
+            neg.insert(r-c);
+            pos.insert(r+c);
+            board[r][c] = 'Q';
+
+            backtrack(r+1, n, col, neg, pos, board, ans);
+
+            col.erase(c);
+            neg.erase(r-c);
+            pos.erase(r+c);
+            board[r][c] = '.';
+        } 
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        unordered_set<int> col, pos, neg;
+
+        vector<string> board;
+        vector<vector<string>> ans;
+
+        for(int r = 0; r < n; r++) {
+            string s = "";
+            for(int c = 0; c < n; c++) s.push_back('.');
+            board.push_back(s);
+        }
+
+        backtrack(0, n, col, neg, pos, board, ans);
+
+        return ans;
+    }
+};   
