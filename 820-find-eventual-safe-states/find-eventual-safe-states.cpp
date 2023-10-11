a6cc00d90ae7a2393333ff63a72d5395745f1c2b
Mohammad Mohaimen Chowdhury <mmc.dp11@gmail.com>
2023-10-12 00:59:58 +0600

    Time: 125 ms (69.43%) | Memory: 47.4 MB (85.38%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,41 +1,29 @@
-/* Topological sort solution */
 class Solution {
+private:
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& safe) {
+        if(safe[node] != -1) return safe[node];
+
+        safe[node] = 0;
+
+        for(int i = 0; i < graph[node].size(); i++) {
+            int v = graph[node][i];
+            if(!dfs(v, graph, safe)) {
+                return safe[node];
+            }
+        }
+
+        safe[node] = 1;
+        return safe[node];
+    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<vector<int>> adj(n);
-        vector<int> indg(n, 0);
+
+        vector<int> safe(n, -1), ans;
 
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < graph[i].size(); j++) {
-                int u = graph[i][j];
-                adj[u].push_back(i);
-                indg[i]++;
-            }
+            if(dfs(i, graph, safe)) ans.push_back(i);
         }
-
-        queue<int> q;
-
-        for(int i = 0; i < n; i++) {
-            if(!indg[i]) q.push(i);
-        }
-
-        vector<int> ans;
-
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-
-            for(int i = 0; i < adj[node].size(); i++) {
-                int v = adj[node][i];
-
-                if(indg[v]) indg[v]--;
-                if(!indg[v]) q.push(v);
-            }
-        }
-
-        sort(ans.begin(), ans.end());
 
         return ans;
     }
