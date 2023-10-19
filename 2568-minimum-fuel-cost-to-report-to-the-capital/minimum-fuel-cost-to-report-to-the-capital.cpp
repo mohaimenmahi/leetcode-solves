935cca9020c20dec176ab1a830783a13dea925a6
Mohammad Mohaimen Chowdhury <mmc.dp11@gmail.com>
2023-10-19 22:37:32 +0600

    Time: 526 ms (24.09%) | Memory: 208.5 MB (31.90%) - LeetSync

---
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    long long ans = 0;
+    int dfs(int node, int prev, int seats, vector<vector<int>>& graph) {
+        int pass = 0;
+        // pass = number of passengers coming to 'node'
+        for(int v:graph[node]) {
+            if(v != prev) {
+                int p = dfs(v, node, seats, graph); 
+                // p = number of passengers coming to 'v'
+                pass += p;
+                ans += (long long) ceil((p * 1.0) / seats);
+                // ceil(p / seats) = number of cars needed to come 'v' taking 'p' passengers
+            }
+        }
+
+        return pass + 1; // number of passengers going from 'node' to it's parent 
+    }
+public:
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = roads.size();
+
+        vector<vector<int>> graph(n+1);
+
+        for(auto road:roads) {
+            graph[road[0]].push_back(road[1]);
+            graph[road[1]].push_back(road[0]);
+        }
+
+        dfs(0, -1, seats, graph);
+
+        return ans;
+    }
+};
