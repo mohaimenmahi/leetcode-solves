40585065c1f3b5c85c467403289925f110f2d8f8
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-01-01 13:10:09 +0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (52.60%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -3,20 +3,17 @@ public:
     int maxScore(string s) {
         int n = s.size();
 
-        vector<int> prefix(n, 0), suffix(n, 0);
+        int left = 0, right = 0;
 
-        prefix[0] = s[0] == '0';
-        suffix[n-1] = s[n-1] == '1';
-
-        for(int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + (s[i] == '0');
-            suffix[n-i-1] = suffix[n-i] + (s[n-i-1] == '1');
-        }
+        for(int i = 0; i < n; i++) if(s[i] == '1') right++;
 
         int ans = INT_MIN;
 
         for(int i = 0; i < n-1; i++) {
-            ans = max(ans, prefix[i] + suffix[i+1]);
+            left += s[i] == '0';
+            right -= s[i] == '1';
+
+            ans = max(ans, left + right);
         }
 
         return ans;
