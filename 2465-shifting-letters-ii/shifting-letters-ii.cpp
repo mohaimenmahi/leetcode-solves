28b85da72a5a146716deb88469afee1a4e1dace4
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-01-05 19:24:27 +0600

    Time: 39 ms (43.47%) | Memory: 109.2 MB (10.53%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+
+        vector<int> diff(n, 0);
+
+        for(auto q:shifts) {
+            int l = q[0], r = q[1], d = q[2];
+            if(d) {
+                diff[l]++;
+                if(r+1 < n) diff[r+1]--;
+            } else {
+                diff[l]--;
+                if(r+1 < n) diff[r+1]++;
+            }
+        }
+
+        int shiftCounts = 0;
+        string res;
+
+        for(int i = 0; i < n; i++) {
+            shiftCounts = (shiftCounts + diff[i]) % 26;
+
+            if(shiftCounts < 26) shiftCounts += 26;
+
+            char newChar = 'a' + (s[i] - 'a' + shiftCounts) % 26;
+            res.push_back(newChar);
+        }
+
+        return res;
+    }
+};
