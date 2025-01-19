8a34f8a4dc1636fb5a2ce409cc8df0e4e3e541a3
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-01-19 13:45:25 +0600

    Time: 71 ms (28.97%) | Memory: 41.3 MB (16.04%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -7,35 +7,29 @@ public:
 
         vector<vector<int>> cost(rows, vector<int>(cols, INT_MAX));
 
-        priority_queue<
-            vector<int>,
-            vector<vector<int>>,
-            greater<vector<int>>
-        > pq;
-
         cost[0][0] = 0;
 
-        pq.push({0, 0, 0});
+        deque<vector<int>> dq;
 
-        while(!pq.empty()) {
-            auto node = pq.top();
-            pq.pop();
+        dq.push_front({0, 0});
 
-            int val = node[0], row = node[1], col = node[2];
+        while(!dq.empty()) {
+            auto front = dq.front();
+            dq.pop_front();
+
+            int row = front[0], col = front[1];
 
             for(int i = 0; i < 4; i++) {
                 int rr = row + dir[i][0], cc = col + dir[i][1];
+                int val = i+1 == grid[row][col] ? 0 : 1;
 
-                if(rr >= 0 && rr < rows && cc >= 0 && cc < cols) {
-                    // if the current cell (row, col) directs to the
-                    // expected neighbor:
-                    // i+1 indicates the direction value, the dir array is
-                    // mapped in the given direction values
-                    int newCost = val + (i+1 != grid[row][col] ? 1 : 0);
+                if(rr >= 0 && rr < rows && cc >= 0 && cc < cols && cost[row][col] + val < cost[rr][cc]) {
+                    cost[rr][cc] = cost[row][col] + val;
 
-                    if(newCost < cost[rr][cc]) {
-                        cost[rr][cc] = newCost;
-                        pq.push({newCost, rr, cc});
+                    if(val == 1) {
+                        dq.push_back({rr, cc});
+                    } else {
+                        dq.push_front({rr, cc});
                     }
                 }
             }
