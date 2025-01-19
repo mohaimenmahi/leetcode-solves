fe5ef4dd7548281abf41c63a34c275c0bed750eb
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-01-19 13:20:14 +0600

    Time: 110 ms (19.13%) | Memory: 30.7 MB (25.18%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,73 +1,44 @@
 class Solution {
+private:
+    vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 public:
     int minCost(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
 
         vector<vector<int>> cost(rows, vector<int>(cols, INT_MAX));
 
+        priority_queue<
+            vector<int>,
+            vector<vector<int>>,
+            greater<vector<int>>
+        > pq;
+
         cost[0][0] = 0;
 
-        while(true) {
-            // copy the prevCost
-            vector<vector<int>> prevCost = cost;
+        pq.push({0, 0, 0});
 
-            // forward pass
-            for(int row = 0; row < rows; row++) {
-                for(int col = 0; col < cols; col++) {
-                    if(row > 0) {
-                        // coming from the top neighbor
-                        cost[row][col] = min(
-                            cost[row][col],
-                            cost[row-1][col] + (
-                                grid[row-1][col] == 3 ? 0 : 1
-                            )
-                        );
-                    }
+        while(!pq.empty()) {
+            auto node = pq.top();
+            pq.pop();
 
-                    if(col > 0) {
-                        // coming from the left neighbor
-                        cost[row][col]= min(
-                            cost[row][col], 
-                            cost[row][col-1] + (
-                                grid[row][col-1] == 1 ? 0 : 1
-                            )
-                        );
+            int val = node[0], row = node[1], col = node[2];
+
+            for(int i = 0; i < 4; i++) {
+                int rr = row + dir[i][0], cc = col + dir[i][1];
+
+                if(rr >= 0 && rr < rows && cc >= 0 && cc < cols) {
+                    // if the current cell (row, col) directs to the
+                    // expected neighbor:
+                    // i+1 indicates the direction value, the dir array is
+                    // mapped in the given direction values
+                    int newCost = val + (i+1 != grid[row][col] ? 1 : 0);
+
+                    if(newCost < cost[rr][cc]) {
+                        cost[rr][cc] = newCost;
+                        pq.push({newCost, rr, cc});
                     }
                 }
             }
-
-            // backward pass
-            for(int row = rows-1; row >= 0; row--) {
-                for(int col = cols-1; col >= 0; col--) {
-                    // from down neighbor
-                    if(row < rows-1) {
-                        cost[row][col] = min(
-                            cost[row][col],
-                            cost[row+1][col] + (
-                                grid[row+1][col] == 4 ? 0 : 1
-                            )
-                        );
-                    }
-
-                    // from right neighbor
-                    if(col < cols-1) {
-                        cost[row][col] = min(
-                            cost[row][col],
-                            cost[row][col+1] + (
-                                grid[row][col+1] == 2 ? 0 : 1
-                            )
-                        );
-                    }
-                }
-            }
-
-            // if the prevCost has not changed in this iteration
-            // that means, in each pass and each cell check from 
-            // all for directions, the values has not been changed
-            // in the cells from the previous iteration
-            // that means we reached to the optimal cost
-
-            if(prevCost == cost) break;
         }
 
         return cost[rows-1][cols-1];
