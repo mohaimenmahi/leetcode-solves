98d19de1075d034119bb3844f59cd827365b8a7e
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-01-21 13:54:48 +0600

    Time: 4 ms (47.51%) | Memory: 66.5 MB (92.76%) - LeetSync

--- 2145-grid-game/grid-game.cpp
+++ 2145-grid-game/grid-game.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
         int n = grid[0].size();
-        vector<vector<long long>> prefix(2, vector<long long>(n, 0));
-
-        prefix[0][0] = (long long) grid[0][0];
-        prefix[1][0] = (long long) grid[1][0];
-
-        for(int i = 1; i < n; i++) {
-            prefix[0][i] = prefix[0][i-1] + (long long) grid[0][i];
-            prefix[1][i] = prefix[1][i-1] + (long long) grid[1][i];
-        } 
-
         long long res = LLONG_MAX;
 
-        for(int i = 0; i < n; i++) {
-            long long first = prefix[0][n-1] - prefix[0][i];
-            long long second = i > 0 ? prefix[1][i-1] : 0;
+        long long firstRow = 0LL, lastRow = 0;
 
-            res = min(res, max(first, second));
+        for(int i = 0; i < n; i++) firstRow += (long long) grid[0][i];
+
+        for(int i = 0; i < n; i++) {
+            firstRow -= (long long) grid[0][i];
+            res = min(res, max(firstRow, lastRow));
+
+            lastRow += grid[1][i];
         }
 
         return res;
