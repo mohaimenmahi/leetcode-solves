e9fa8a7cd929250bd8302c73b50913b17b937a87
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-01-22 12:54:27 +0600

    Time: 102 ms (24.03%) | Memory: 128.2 MB (26.94%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    bool isValid(int row, int col, int m, int n) {
+        return row >= 0 && row < m && col >= 0 && col < n;
+    }
+
+    vector<vector<int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        vector<vector<int>> peaks(m, vector<int>(n, 0));
+
+        queue<pair<int, pair<int, int>>> q;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(isWater[i][j]) {
+                    q.push({0, {i, j}});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        while(!q.empty()) {
+            auto node = q.front();
+            q.pop();
+
+            int val = node.first, row = node.second.first, col = node.second.second;
+
+            for(int i = 0; i < 4; i++) {
+                int rr = row + dir[i][0], cc = col + dir[i][1];
+
+                if(isValid(rr, cc, m, n) && !vis[rr][cc]) {
+                    peaks[rr][cc] = val + 1;
+                    q.push({val+1, {rr, cc}});
+                    vis[rr][cc] = 1;
+                }
+            }
+        }
+
+        return peaks;
+    }
+};
