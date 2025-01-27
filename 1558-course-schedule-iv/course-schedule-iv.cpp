5bf73c0bd423e424e681d28c01d7ac759e68f718
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-01-27 21:14:51 +0600

    Time: 104 ms (30.10%) | Memory: 63.9 MB (93.96%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,56 +1,31 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(
-        int numCourses, 
-        vector<vector<int>>& prerequisites, 
-        vector<vector<int>>& queries
+        int num, 
+        vector<vector<int>>& preqs, 
+        vector<vector<int>>& qrs
     ) {
-        vector<vector<int>> graph(numCourses);
-        vector<int> indgr(numCourses, 0);
+        vector<vector<bool>> hasPath(num, vector<bool>(num, false));
 
-        for(int i = 0; i < prerequisites.size(); i++) {
-            int u = prerequisites[i][0], v = prerequisites[i][1];
-            indgr[v]++;
-            graph[u].push_back(v);
+        for(int i = 0; i < preqs.size(); i++) {
+            int u = preqs[i][0], v = preqs[i][1];
+            hasPath[u][v] = true; // means from u to v, there is a path
         }
 
-        queue<int> q;
-
-        for(int i = 0; i < numCourses; i++) {
-            if(indgr[i] == 0) q.push(i);
-        }
-
-        unordered_map<int, unordered_set<int>> nodePrereqs;
-
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            for(int i:graph[node]) {
-                // nodePrereqs[i] => the prerequisites of i
-                nodePrereqs[i].insert(node);
-
-                // nodePrereqs[node] => the prerequisites of node
-                for(int it:nodePrereqs[node]) {
-                    // inserting all the prerequisites of node as prereqs of i
-                    nodePrereqs[i].insert(it);
-                } 
-
-                indgr[i]--;
-
-                if(indgr[i] == 0) {
-                    q.push(i);
+        for(int k = 0; k < num; k++) {
+            for(int i = 0; i < num; i++) {
+                for(int j = 0; j < num; j++) {
+                    hasPath[i][j] = hasPath[i][j] || (hasPath[i][k] && hasPath[k][j]);
                 }
             }
         }
 
-        vector<bool> res(queries.size(), false);
+        vector<bool> res;
 
-        for(int i = 0; i < queries.size(); i++) {
-            int u = queries[i][0], v = queries[i][1];
+        for(int i = 0; i < qrs.size(); i++) {
+            int u = qrs[i][0], v = qrs[i][1];
 
-            if(nodePrereqs.find(v) != nodePrereqs.end() && nodePrereqs[v].find(u) != nodePrereqs[v].end()) 
-                res[i] = true;
+            res.push_back(hasPath[u][v]);
         }
 
         return res;
