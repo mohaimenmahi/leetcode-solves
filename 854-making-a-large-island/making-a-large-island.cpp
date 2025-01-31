fa9674fdbd828bb3f2d26716bba2d53fd028d885
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-02-01 01:47:26 +0600

    Time: 531 ms (18.90%) | Memory: 235.8 MB (14.29%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,112 +1,73 @@
-class DSU {
-public:
-    vector<int> parent, rank;
-    DSU(int n) {
-        for(int i = 0; i < n; i++) {
-            rank.push_back(1);
-            parent.push_back(i);
-        }
-    }
-
-    int findUP(int node) {
-        int p = parent[node];
-
-        while(p != parent[p]) {
-            parent[p] = parent[parent[p]];
-            p = parent[p];
-        }
-        parent[node] = p;
-        return p;
-    }
-
-    void makeUnion(int u, int v) {
-        int p1 = findUP(u);
-        int p2 = findUP(v);
-
-        if(p1 == p2) return;
-
-        if(rank[p1] >= rank[p2]) {
-            parent[p2] = p1;
-            rank[p1] += rank[p2];
-        } else {
-            parent[p1] = p2;
-            rank[p2] += rank[p1];
-        }
-    }
-
-    int countSize(int p) {
-        return rank[p];
-    }
-};
-
 class Solution {
 private:
-    int dx[4] = {1, -1, 0, 0};
-    int dy[4] = {0, 0, 1, -1};
-
-    void dfs(
-        DSU* ds,
-        int n, 
+    int dfs(
         int row, 
         int col, 
-        vector<vector<int>>& vis, 
+        int landId,
+        set<pair<int, int>>& vis, 
         vector<vector<int>>& grid
     ) {
-        vis[row][col] = 1;
-        int node = (row * n) + col;
+        if(row < 0 || row == grid.size() || col < 0 || col == grid[0].size())
+            return 0;
         
-        for(int i = 0; i < 4; i++) {
-            int r = row + dx[i];
-            int c = col + dy[i];
+        if(grid[row][col] != 1 || vis.count({row, col})) return 0;
+        
+        vis.insert({row, col});
 
-            if(r < 0 || c < 0 || r == n || c == n || vis[r][c] || !grid[r][c]) continue;
+        int count = 1;
 
-            int adNode = (r*n) + c;
+        grid[row][col] = landId;
 
-            ds->makeUnion(node, adNode);
-            dfs(ds, n, r, c, vis, grid);
-        }
+        count += dfs(row-1, col, landId, vis, grid);
+        count += dfs(row+1, col, landId, vis, grid);
+        count += dfs(row, col-1, landId, vis, grid);
+        count += dfs(row, col+1, landId, vis, grid);
+
+        return count;
     }
 public:
     int largestIsland(vector<vector<int>>& grid) {
         int n = grid.size();
-        
-        DSU* ds = new DSU(n*n);
+        set<pair<int, int>> vis;
+        unordered_map<int, int> mp;
+        int res = 0;
 
-        vector<vector<int>> vis(n, vector<int>(n, 0));
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(!vis[i][j] && grid[i][j]) dfs(ds, n, i, j, vis, grid);
-            }
-        }
-
-        int ans = 0;
+        int landId = 2;
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(!vis[i][j] && !grid[i][j]) {
-                    vis[i][j] = 1;
-                    int res = 1;
-                    unordered_set<int> st;
-                    for(int k = 0; k < 4; k++) {
-                        int x = i + dx[k];
-                        int y = j + dy[k];
-
-                        if(x < 0 || y < 0 || x == n || y == n || !grid[x][y]) continue;
-
-                        int node = (x * n) + y;
-                        int p = ds->findUP(node);
-                        if(st.count(p) == 0) {
-                            res += ds->countSize(p);
-                            st.insert(p);
-                        }
-                    }
-                    ans = max(res, ans);
+                if(grid[i][j] == 1) {
+                    mp[landId] = dfs(i, j, landId, vis, grid);
+                    landId++;
                 }
             }
         }
 
-        return ans > 0 ? ans : n*n;
+        if(!mp.size()) return 1;
+
+        vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!grid[i][j]) {
+                    unordered_set<int> visIsland;
+                    for(int k = 0; k < 4; k++) {
+                        int row = i + dir[k][0], col = j + dir[k][1];
+
+                        if(row >= 0 && row < n && col >= 0 && col < n) {
+                            int island = grid[row][col];
+
+                            visIsland.insert(island);
+                        }
+                    }
+                    int cnt  = 1;
+                    for(int it:visIsland) cnt += mp[it];
+
+                    res = max(res, cnt);
+                }
+            }
+        }
+
+        return res > 1 ? res : n * n;
     }
 };
