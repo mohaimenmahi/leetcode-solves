ca2089514f3d8b552fb763d4a12df14fdd8e1c87
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-02-02 16:28:01 +0600

    Time: 0 ms (100.00%) | Memory: 11.1 MB (58.89%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,36 +1,16 @@
 class Solution {
-private:
-    bool isSorted(int start, vector<int>& nums) {
-        int idx = 0;
-        int n = nums.size();
-        int mini = nums[start];
-        while((start + idx) % n < n && idx < n) {
-            int pos = (start + idx) % n;
-            if(mini > nums[pos]) return false;
-            
-            mini = nums[pos];
-            idx++;
-        }
-
-        return true;
-    }
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
 
-        int  mini = 101;
+        int inversions = 0;
 
-        for(int i = 0; i < n; i++) mini = min(mini, nums[i]);
-
-        unordered_set<int> minIdxs;
-        for(int i = 0; i < n; i++) {
-            if(mini == nums[i]) minIdxs.insert(i);
+        for(int i = 1; i < n; i++) {
+            if(nums[i] < nums[i-1]) inversions++;
         }
 
-        for(int i:minIdxs) {
-            if(isSorted(i, nums)) return true;
-        }
+        if(nums[0] < nums[n-1]) inversions++;
 
-        return false;
+        return inversions <= 1;
     }
 };
