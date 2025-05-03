8d94f17d15f7eb594127d146408f4bee2aa38892
Mohaimen <28109132+mohaimenmahi@users.noreply.github.com>
2025-05-03 16:53:59 +0600

    Time: 19 ms (49.82%) | Memory: 22.8 MB (43.77%) - LeetSync

--- 868-push-dominoes/push-dominoes.cpp
+++ 868-push-dominoes/push-dominoes.cpp
@@ -2,42 +2,31 @@ class Solution {
 public:
     string pushDominoes(string dominoes) {
         int n = dominoes.size();
-        
-        vector<int> indexes(n+2);
-        vector<char> symbols(n+2);
 
-        int len = 1;
-        indexes[0] = -1;
-        symbols[0] = 'L';
+        vector<int> forces(n, 0);
+
+        int force = 0;
+        for(int i = 0; i < n; i++) {
+            if(dominoes[i] == 'R') force = n;
+            else if(dominoes[i] == 'L') force = 0;
+            else force = max(force-1, 0);
+            forces[i] += force;
+        }
+
+        force = 0;
+        for(int i = n-1; i >= 0; i--) {
+            if(dominoes[i] == 'L') force = n;
+            else if(dominoes[i] == 'R') force = 0;
+            else force = max(force-1, 0);
+            forces[i] -= force;
+        }
+
+        string ans;
 
         for(int i = 0; i < n; i++) {
-            if(dominoes[i] != '.') {
-                indexes[len] = i;
-                symbols[len] = dominoes[i];
-                len++;
-            }
+            ans.push_back(forces[i] > 0 ? 'R' : forces[i] < 0 ? 'L' : '.');
         }
 
-        indexes[len] = n;
-        symbols[len++] = 'R';
-
-        vector<char> ans(dominoes.begin(), dominoes.end());
-
-        for(int idx = 0; idx < len - 1; idx++) {
-            int i = indexes[idx], j = indexes[idx+1];
-            char x = symbols[idx], y = symbols[idx+1];
-
-            if(x == y) {
-                for(int k = i+1; k < j; k++) ans[k] = x;
-            } else if(x > y) {
-                for(int k = i+1; k < j; k++) {
-                    ans[k] = k-i == j-k ? '.' : k-i > j-k ? 'L' : 'R';
-                }
-            }
-        }
-
-        string res(ans.begin(), ans.end());
-
-        return res;
+        return ans;
     }
 };
